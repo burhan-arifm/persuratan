47622086076a613d2af568c719d1acad4e1c3bee
Burhan Arif M <burhan.arifm@hotmail.com>
2022-02-12 17:26:29 +0700

    tidying whitespaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,19 +9,19 @@
 
     <!--Page Title-->
     <title>@yield('page-title')</title>
-    
+
     <!--Favicon-->
-    <link rel="icon" type="image/png" href="{{ asset('storage/favicon.ico') }}"> 
-    
+    <link rel="icon" type="image/png" href="{{ asset('storage/favicon.ico') }}">
+
     <!--CSS Files Dependencies-->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('additional-css')
 
     <!--[if lt IE 9]>
 	<script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>	
+	<script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
     <![endif]-->
-    
+
 </head>
 <body>
     @yield('body')
