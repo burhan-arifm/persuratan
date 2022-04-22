7fbf5c516de983182c9df4f7d737ef0bee6a0f3e
Burhan Arif M <burhan.arifm@hotmail.com>
2022-04-22 21:39:04 +0700

    tidying git index

--- resources/js/ziggy.js
+++
@@ -1,9 +0,0 @@
-const Ziggy = {"url":"http:\/\/127.0.0.1:8000","port":8000,"defaults":{},"routes":{"logout":{"uri":"logout","methods":["POST"]},"surat.detail":{"uri":"surat\/{id}","methods":["GET","HEAD"]},"surat.cetak":{"uri":"surat\/{id}\/cetak","methods":["GET","HEAD"]},"surat.sunting":{"uri":"surat\/{id}\/sunting","methods":["GET","HEAD"]},"surat.hapus":{"uri":"surat\/{id}","methods":["DELETE"]},"pengaturan.admin.reset":{"uri":"pengaturan\/admin\/{id}\/password-reset","methods":["GET","HEAD"]},"pengaturan.admin.hapus":{"uri":"pengaturan\/admin\/{id}","methods":["DELETE"]}}};
-
-if (typeof window !== 'undefined' && typeof window.Ziggy !== 'undefined') {
-    for (let name in window.Ziggy.routes) {
-        Ziggy.routes[name] = window.Ziggy.routes[name];
-    }
-}
-
-export { Ziggy };
